58237dd70247565ffdaf9ab2ab48fbee533885a2
semyon422 <semyon422@gmail.com>
2019-03-10 12:41:32 +0000

    Some code was rewritten and json output was added

--- README.md
+++ README.md
@@ -4,13 +4,16 @@ osu!mania performance points (and starrate) calculator
 ```
 Usage: lua omppc.lua [OPTIONS]
 
-  -b, --beatmap              set path to .osu file  
-  -m, --mods                 set mods in AABB..ZZ format (e.g. EZNFFL)  
-  -s, --score                set score  
-  -a, --accuracy             set accuracy  
-  -v, --verbose              set verbose mode  
+  -b    set path to .osu file  
+  -m    set mods in AABB..ZZ format (e.g. EZNFFL) or number  
+  -s    set score  
+  -v    set verbose mode  
+  -j    set json mode  
 
 Examples: 
-  lua omppc.lua -b /path/to/file.osu -s 1000000 -a 100
-  lua omppc.lua --beatmap /path/to/file.osu --score 500000 --accuracy 100 --mods EZDT --verbose
+  lua omppc.lua -b /path/to/file.osu -s 1000000
+  omppc.lua -b /path/to/file.osu -s 1000000
+  omppc.lua -b "/path with spaces/to/file.osu" -s 1000000
+  omppc.lua -b /path/to/file.osu -s 500000 -m EZDT -v
+  omppc.lua -b map.osu -s 500000 -m 64 -j
 ```
--- omppc.lua
+++ omppc.lua
@@ -1,17 +1,4 @@
-string.split = function(self, divider, notPlain)
-	local position = 0
-	local output = {}
-	
-	for endchar, startchar in function() return self:find(divider, position, not notPlain) end do
-		table.insert(output, self:sub(position, endchar - 1))
-		position = startchar + 1
-	end
-	table.insert(output, self:sub(position))
-	
-	return output
-end
-
-Class = {}
+local Class = {}
 
 Class.new = function(self, object)
 	local object = object or {}
@@ -22,29 +9,36 @@ Class.new = function(self, object)
 	return object
 end
 
-Mods = Class:new()
+local Mods = Class:new()
 
-Mods.parse = function(self, modsString)
-	if not modsString then
-		modsString = ""
+Mods.scoreRate = 1
+Mods.timeRate = 1
+Mods.odRate = 1
+
+Mods.parse = function(self, mods)
+	self.modsData = mods
+	if not mods then
+		return
+	elseif tonumber(mods) then
+		return self:parseNumber(tonumber(mods))
+	else
+		return self:parseString(mods)
 	end
-	self.modsString = modsString
-	
-	self.scoreMultiplier = 1
-	self.timeRate = 1
-	self.overallDifficultyMultiplier = 1
+end
+
+Mods.parseString = function(self, modsString)
 	if modsString:find("EZ") then
 		self.Easy = true
-		self.scoreMultiplier = self.scoreMultiplier * 0.5
-		self.overallDifficultyMultiplier = 0.5
+		self.scoreRate = self.scoreRate * 0.5
+		self.odRate = 0.5
 	end
 	if modsString:find("NF") then
 		self.NoFail = true
-		self.scoreMultiplier = self.scoreMultiplier * 0.5
+		self.scoreRate = self.scoreRate * 0.5
 	end
 	if modsString:find("HT") then
 		self.HalfTime = true
-		self.scoreMultiplier = self.scoreMultiplier * 0.5
+		self.scoreRate = self.scoreRate * 0.5
 		self.timeRate = 3/4
 	end
 	if modsString:find("DT") then
@@ -55,30 +49,22 @@ Mods.parse = function(self, modsString)
 	return self
 end
 
-Mods.parseBitwise = function(self, bitwise)
-	if not modsString then
-		modsString = ""
-	end
-	self.modsString = modsString
-	
-	self.scoreMultiplier = 1
-	self.timeRate = 1
-	self.overallDifficultyMultiplier = 1
-	if bit.band(bitwise, 2) == 2 then
+Mods.parseNumber = function(self, modsNumber)
+	if bit.band(modsNumber, 2) == 2 then
 		self.Easy = true
-		self.scoreMultiplier = self.scoreMultiplier * 0.5
-		self.overallDifficultyMultiplier = 0.5
+		self.scoreRate = self.scoreRate * 0.5
+		self.odRate = 0.5
 	end
-	if bit.band(bitwise, 1) == 1 then
+	if bit.band(modsNumber, 1) == 1 then
 		self.NoFail = true
-		self.scoreMultiplier = self.scoreMultiplier * 0.5
+		self.scoreRate = self.scoreRate * 0.5
 	end
-	if bit.band(bitwise, 256) == 256 then
+	if bit.band(modsNumber, 256) == 256 then
 		self.HalfTime = true
-		self.scoreMultiplier = self.scoreMultiplier * 0.5
+		self.scoreRate = self.scoreRate * 0.5
 		self.timeRate = 3/4
 	end
-	if bit.band(bitwise, 64) == 64 then
+	if bit.band(modsNumber, 64) == 64 then
 		self.DoubleTime = true
 		self.timeRate = 3/2
 	end
@@ -86,24 +72,25 @@ Mods.parseBitwise = function(self, bitwise)
 	return self
 end
 
-Note = Class:new()
+local Note = Class:new()
 
-Note.parse = function(self, line)
+Note.parse = function(self, line, keymode)
 	local x, startTime = line:match("^(%d-),%d-,(%d-),")
 	local endTime = line:match(",(%d-):.+:.+:.+:.+:")
 	
-	self.x = tonumber(x)
-	
-	local interval = 512 / self.beatmap.keymode
-	for newKey = 1, self.beatmap.keymode do
-		if self.x >= interval * (newKey - 1) and self.x < newKey * interval then
-			self.key = newKey
-			break
-		end
-	end
+	self.key = math.ceil(x / 512 * keymode)
+	self.keymode = keymode
 	
 	self.startTime = tonumber(startTime)
 	self.endTime = tonumber(endTime) or self.startTime
+	
+	self.heldUntil = {}
+	self.individualStrains = {}
+	
+	for i = 1, keymode do
+		self.individualStrains[i] = 0
+		self.heldUntil[i] = 0
+	end
 	
 	return self
 end
@@ -112,16 +99,6 @@ Note.INDIVIDUAL_DECAY_BASE = 0.125
 Note.OVERALL_DECAY_BASE = 0.30
 
 Note.overallStrain = 1
-
-Note.init = function(self)
-	self.heldUntil = {}
-	self.individualStrains = {}
-	
-	for i = 1, self.beatmap.keymode do
-		self.individualStrains[i] = 0
-		self.heldUntil[i] = 0
-	end
-end
 
 Note.getIndividualStrain = function(self)
 	return self.individualStrains[self.key]
@@ -140,75 +117,61 @@ Note.calculateStrains = function(self, pNote, timeRate)
 	local holdFactor = 1
 	local holdAddition = 0
 	
-	for i = 1, self.beatmap.keymode do
+	for i = 1, self.keymode do
 		self.heldUntil[i] = pNote.heldUntil[i]
-
+		
 		if self.startTime < self.heldUntil[i] and self.endTime > self.heldUntil[i] then
 			holdAddition = 1
 		end
-
 		if self.endTime == self.heldUntil[i] then
 			holdAddition = 0
 		end
-
 		if self.heldUntil[i] > self.endTime then
 			holdFactor = 1.25
 		end
 	end
 	self.heldUntil[self.key] = self.endTime
 	
-	for i = 1, self.beatmap.keymode do
+	for i = 1, self.keymode do
 		self.individualStrains[i] = pNote.individualStrains[i] * individualDecay
 	end
 	self:setIndividualStrain(self:getIndividualStrain() + 2 * holdFactor)
-
+	
 	self.overallStrain = pNote.overallStrain * overallDecay + (addition + holdAddition) * holdFactor
 end
 
-Beatmap = Class:new()
+local Beatmap = Class:new()
 
 Beatmap.parse = function(self, beatmapString)
     self.noteData = {}
-
-    local blockName = ""
-	for _, line in ipairs(beatmapString:split("\n")) do
+	
+    local blockName
+	for line in string.gmatch(beatmapString .. "\n", "(.-)\n") do
 		line = line:match("^%s*(.-)%s*$")
         if line:find("^%[") then
             blockName = line:match("^%[(.*)%]")
-        elseif (blockName == "General" or blockName == "Difficulty") and not line:match("^%s*$") then
+        elseif blockName == "General" or blockName == "Difficulty" then
 			if line:match("^Mode:") then
 				self.mode = tonumber(line:match(":(%d+)$"))
 			elseif line:match("^OverallDifficulty") then
-				self.overallDifficulty = tonumber(line:match(":(.+)$"))
-			elseif line:match("^HPDrainRate") then
-				self.healthPoints = tonumber(line:match(":(.+)$"))
+				self.od = tonumber(line:match(":(.+)$"))
             elseif line:match("^CircleSize") then
 				self.keymode = tonumber(line:match(":(.+)$"))
             end
         elseif blockName == "HitObjects" and not line:match("^%s*$") then
-			local note = Note:new({beatmap = self})
-			note:parse(line)
-			note:init()
-            table.insert(self.noteData, note)
+            self.noteData[#self.noteData + 1] = Note:new():parse(line, self.keymode)
         end
     end
 	table.sort(self.noteData, function(a, b) return a.startTime < b.startTime end)
-	
 	self.noteCount = #self.noteData
+	
+	self:calculateStarRate()
 	
 	return self
 end
 
-Beatmap.getMaximumScore = function(self)
-	return 1000000 * self.mods.scoreMultiplier
-end
-
-Beatmap.getOverallDifficulty = function(self)
-	return self.overallDifficulty * self.mods.overallDifficultyMultiplier
-end
-
-Beatmap.getHealthPoints = function(self)
-	return self.healthPoints * self.mods.overallDifficultyMultiplier
+Beatmap.getOD = function(self)
+	return self.od * self.mods.odRate
 end
 
 Beatmap.STAR_SCALING_FACTOR = 0.018
@@ -219,7 +182,7 @@ Beatmap.calculateStarRate = function(self)
 end
 
 Beatmap.getStarRate = function(self)
-	return self.starRate or self:calculateStarRate() or self.starRate
+	return self.starRate
 end
 
 Beatmap.calculateStrainValues = function(self)
@@ -238,55 +201,53 @@ Beatmap.DECAY_WEIGHT = 0.9
 
 Beatmap.calculateDifficulty = function(self)
 	local actualStrainStep = self.STRAIN_STEP * self.mods.timeRate
-
+	
 	local highestStrains = {}
 	local intervalEndTime = actualStrainStep
 	local maximumStrain = 0
-
-	local previousNote
+	
+	local pNote
 	for _, note in ipairs(self.noteData) do
 		while (note.startTime > intervalEndTime) do
 			table.insert(highestStrains, maximumStrain)
-			if not previousNote then
+			if not pNote then
 				maximumStrain = 0
 			else
-				local individualDecay = math.pow(note.INDIVIDUAL_DECAY_BASE, (intervalEndTime - previousNote.startTime) / 1000)
-				local overallDecay = math.pow(note.OVERALL_DECAY_BASE, (intervalEndTime - previousNote.startTime) / 1000)
-				maximumStrain = previousNote:getIndividualStrain() * individualDecay + previousNote.overallStrain * overallDecay
+				local individualDecay = math.pow(note.INDIVIDUAL_DECAY_BASE, (intervalEndTime - pNote.startTime) / 1000)
+				local overallDecay = math.pow(note.OVERALL_DECAY_BASE, (intervalEndTime - pNote.startTime) / 1000)
+				maximumStrain = pNote:getIndividualStrain() * individualDecay + pNote.overallStrain * overallDecay
 			end
-
+			
 			intervalEndTime = intervalEndTime + actualStrainStep
 		end
-
+		
 		local strain = note:getIndividualStrain() + note.overallStrain
 		if strain > maximumStrain then
 			maximumStrain = strain
 		end
-
-		previousNote = note
+		
+		pNote = note
 	end
-
+	
 	local difficulty = 0
 	local weight = 1
 	table.sort(highestStrains, function(a, b) return a > b end)
-
+	
 	for _, strain in ipairs(highestStrains) do
 		difficulty = difficulty + weight * strain
 		weight = weight * self.DECAY_WEIGHT
 	end
-
+	
 	return difficulty
 end
 
-OsuManiaPerformanceCalculator = Class:new()
+local Calculator = Class:new()
 
-OsuManiaPerformanceCalculator.computeTotalValue = function(self)
+Calculator.computeTotalValue = function(self)
 	local multiplier = 0.8
+	
 	if self.mods.NoFail then
 		multiplier = multiplier * 0.90
-	end
-	if self.mods.SpunOut then
-		multiplier = multiplier * 0.95
 	end
 	if self.mods.Easy then
 		multiplier = multiplier * 0.50
@@ -294,21 +255,21 @@ OsuManiaPerformanceCalculator.computeTotalValue = function(self)
 	
 	self:computeStrainValue()
 	self:computeAccValue()
-
+	
 	self.totalValue = math.pow(math.pow(self.strainValue, 1.1) + math.pow(self.accValue, 1.1), 1 / 1.1) * multiplier
 end
 
-OsuManiaPerformanceCalculator.computeStrainValue = function(self)
-	if self.mods.scoreMultiplier <= 0 then
+Calculator.computeStrainValue = function(self)
+	if self.mods.scoreRate <= 0 then
 		self.strainValue = 0
 		return
 	end
-
-	self.realScore = self.score * (1 / self.mods.scoreMultiplier)
+	
+	self.realScore = self.score * (1 / self.mods.scoreRate)
 	local score = self.realScore
-
-	self.strainValue = math.pow(5 * math.max(1, self.starRate / 0.2) - 4, 2.2) / 135
-	self.strainValue = self.strainValue * (1 + 0.1 * math.min(1, self.noteCount / 1500))
+	
+	self.strainValue = math.pow(5 * math.max(1, self.beatmap:getStarRate() / 0.2) - 4, 2.2) / 135
+	self.strainValue = self.strainValue * (1 + 0.1 * math.min(1, self.beatmap.noteCount / 1500))
 	
 	if score <= 500000 then
 		self.strainValue = 0
@@ -325,8 +286,8 @@ OsuManiaPerformanceCalculator.computeStrainValue = function(self)
 	end
 end
 
-OsuManiaPerformanceCalculator.computeAccValue = function(self)
-	local hitWindow300 = 34 + 3 * (math.min(10, math.max(0, 10 - self.overallDifficulty)))
+Calculator.computeAccValue = function(self)
+	local hitWindow300 = 34 + 3 * (math.min(10, math.max(0, 10 - self.beatmap.od)))
 	if hitWindow300 <= 0 then
 		self.accValue = 0
 		return
@@ -335,121 +296,148 @@ OsuManiaPerformanceCalculator.computeAccValue = function(self)
 	self.accValue = math.max(0, 0.2 - ((hitWindow300 - 34) * 0.006667)) * self.strainValue * math.pow((math.max(0, self.realScore - 960000) / 40000), 1.1)
 end
 
-PlayData = Class:new()
+local PlayData = Class:new()
 
-PlayData.computePerformancePoints = function(self)
+PlayData.process = function(self)
+	self.mods = Mods:new():parse(self.modsData)
+	
+	self.beatmap = Beatmap:new()
 	self.beatmap.mods = self.mods
-	self.pCalc = OsuManiaPerformanceCalculator:new({
+	self.beatmap:parse(self.beatmapString)
+	
+	self.calculator = Calculator:new({
 		beatmap = self.beatmap,
-		starRate = self.beatmap:getStarRate(),
-		noteCount = self.beatmap.noteCount,
-		overallDifficulty = self.beatmap:getOverallDifficulty(),
-		
 		score = self.score,
-		accuracy = self.accuracy,
 		mods = self.mods
 	})
-	self.pCalc:computeTotalValue()
-	self.performancePoints = self.pCalc.totalValue
+	self.calculator:computeTotalValue()
 end
 
-PlayData.getPerformancePoints = function(self)
-	return self.performancePoints or self:computePerformancePoints() or self.performancePoints
+PlayData.getData = function(self)
+	return {
+		modsData = self.mods.modsData,
+		scoreRate = self.mods.scoreRate,
+		timeRate = self.mods.timeRate,
+		odRate = self.mods.odRate,
+		starRate = self.beatmap.starRate,
+		noteCount = self.beatmap.noteCount,
+		scaledOD = self.beatmap:getOD(),
+		od = self.beatmap.od,
+		score = self.score,
+		realScore = self.calculator.realScore,
+		strainValue = self.calculator.strainValue,
+		accValue = self.calculator.accValue,
+		pp = self.calculator.totalValue
+	}
 end
 
-PlayData.getAccuracyFromHits = function(self, numMiss, num50, num100, numKatu, num300, numGeki)
-	local totalHits = numMiss + num50 + num100 + numKatu + num300 + numGeki
-	return (num50 * 50 + num100 * 100 + numKatu * 200 + (num300 + numGeki) * 300) / (totalHits * 300)
+PlayData.getJSON = function(self)
+	local data = self:getData()
+	
+	local out = {}
+	out[1] = "{"
+	for key, value in pairs(data) do
+		out[#out + 1] = "\"" .. key .. "\":"
+		if type(value) == "number" then
+			out[#out + 1] = value .. ","
+		elseif type(value) == "string" then
+			out[#out + 1] = "\"" .. value .. "\","
+		end
+	end
+	out[#out] = out[#out]:sub(1, -2)
+	out[#out + 1] = "}"
+	
+	return table.concat(out)
 end
 
-if arg then
+if arg and arg[0] and arg[0]:find("omppc%.lua$") then
 	input = {}
-
+	
 	for i = 1, #arg do
 		local cArg = arg[i]
 		local nArg = arg[i + 1]
 		
 		if cArg:match("^%-") then
-			local key
-			if cArg:match("^%-%-") then
-				key = cArg:match("^%-%-(.*)$")
-			else
-				key = cArg:match("^%-(.*)$")
-			end
+			local key = cArg:match("^%-(.*)$")
 			
-			if key == "beatmap" or key == "b" then
-				input.beatmapPath = nArg
-			elseif key == "mods" or key == "m" then
+			if key == "b" then
+				input.path = nArg
+			elseif key == "m" then
 				input.mods = nArg
-			elseif key == "score" or key == "s" then
+			elseif key == "s" then
 				input.score = tonumber(nArg)
-			elseif key == "accuracy" or key == "a" then
-				input.accuracy = tonumber(nArg) / 100
-			elseif key == "verbose" or key == "v" then
+			elseif key == "v" then
 				input.verbose = true
+			elseif key == "j" then
+				input.json = true
 			end
 			
-			i = i + 1
+			i = i + 2
 		end
 	end
 	
-	local file = io.open(input.beatmapPath, "r")
+	local file = io.open(input.path, "r")
 	
-	playData = PlayData:new()
-	playData.mods = Mods:new():parse(input.mods)
-
-	playData.beatmap = Beatmap:new()
-	playData.beatmap:parse(file:read("*a"))
-	file:close()
-	playData.beatmap.mods = playData.mods
-
+	local playData = PlayData:new()
+	playData.modsData = input.mods
+	playData.beatmapString = file:read("*a")
 	playData.score = input.score
-	playData.accuracy = input.accuracy
-
+	playData:process()
+	
+	file:close()
+	
+	local data = playData:getData()
+	
 	if input.verbose then
-		playData:computePerformancePoints()
 		print(
 			([[
 Mods info
- modsString   %8s
- scoreMult    %8.2f
- timeRate     %8.2f
- odMult       %8.2f
+  mods         %8s
+  scoreRate    %8.2f
+  timeRate     %8.2f
+  odRate       %8.2f
 Beatmap info:
- starRate     %8.2f
- noteCount    %8d
- scaled OD    %8.1f
- real OD      %8.1f
- scaled HP    %8.1f
- real HP      %8.1f
+  starRate     %8.2f
+  noteCount    %8d
+  scaled OD    %8.1f
+  real OD      %8.1f
 Play info
- scaled score %8d
- real score   %8d
- accuracy     %8.2f
- strainValue  %8.2f
- accValue     %8.2f
- PP           %8.2f
+  scaled score %8d
+  real score   %8d
+  strainValue  %8.2f
+  accValue     %8.2f
+  PP           %8.2f
 ]]
-			):format(	
-				playData.mods.modsString,
-				playData.mods.scoreMultiplier,
-				playData.mods.timeRate,
-				playData.mods.overallDifficultyMultiplier,
-				playData.beatmap:getStarRate(),
-				playData.beatmap.noteCount,
-				playData.beatmap:getOverallDifficulty(),
-				playData.beatmap.overallDifficulty,
-				playData.beatmap:getHealthPoints(),
-				playData.beatmap.healthPoints,
-				input.score,
-				playData.pCalc.realScore,
-				input.accuracy * 100,
-				playData.pCalc.strainValue,
-				playData.pCalc.accValue,
-				playData:getPerformancePoints()
+			):format(
+				data.modsData,
+				data.scoreRate,
+				data.timeRate,
+				data.odRate,
+				data.starRate,
+				data.noteCount,
+				data.scaledOD,
+				data.od,
+				data.score,
+				data.realScore,
+				data.strainValue,
+				data.accValue,
+				data.pp
 			)
 		)
+	elseif input.json then
+		print(playData:getJSON())
 	else
-		print(playData:getPerformancePoints())
+		print(data.pp)
 	end
+	
+	return
 end
+
+return {
+	Class = Class,
+	Mods = Mods,
+	Note = Note,
+	PlayData = PlayData,
+	Beatmap = Beatmap,
+	Calculator = Calculator
+}
